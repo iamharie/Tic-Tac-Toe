209e253c532b605e658d0cfae684aba267d77846
iamharie <haranhari350@gmail.com>
2024-08-03 19:53:22 -0500

    activePlayer state terminated, it is now being handelled by gameTurns state

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import { useState } from "react";
+
+function derivedState(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+  let activePlayer = derivedState(gameTurns);
+
   function handleSquareSelect(rowIndex, colIndex) {
-    setActivePlayer((currActivePlayer) =>
-      currActivePlayer === "X" ? "O" : "X"
-    );
+    // setActivePlayer((currActivePlayer) =>
+    //   currActivePlayer === "X" ? "O" : "X"
+    // );
     //gameTurns
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      let currentPlayer = derivedState(prevTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
